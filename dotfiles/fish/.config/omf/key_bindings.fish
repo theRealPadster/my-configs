5329b6d9128def4ed1a08028a6407fff9d33e1a9
theRealPadster <padspambox@gmail.com>
2018-11-04 20:44:47 -0500

    Added !! and !$ keybinds to fish config

---
+++ dotfiles/fish/.config/omf/key_bindings.fish
@@ -0,0 +1,22 @@
+function bind_bang
+  switch (commandline -t)
+  case "!"
+    commandline -t $history[1]; commandline -f repaint
+  case "*"
+    commandline -i !
+  end
+end
+
+function bind_dollar
+  switch (commandline -t)
+  case "!"
+    commandline -t ""
+    commandline -f history-token-search-backward
+  case "*"
+    commandline -i '$'
+  end
+end
+
+bind ! bind_bang
+bind '$' bind_dollar
+
