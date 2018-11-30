af59fe3240b6ea056796c124b5bec086db609efc
Isaac Maier <padspambox@gmail.com>
2018-11-30 16:46:16 -0500

    Add (WIP) screen config

--- stow-all.sh
+++ stow-all.sh
@@ -3,6 +3,6 @@
 # cd into dotfiles folder
 cd dotfiles;
 # stow $HOME config files
-stow bash fish misc moc templates tilix top zsh --target="$HOME";
+stow bash fish misc moc screen templates tilix top zsh --target="$HOME";
 # stow other stuff?
 # sudo stow mouse --target="/etc/X11/xorg.conf.d"
